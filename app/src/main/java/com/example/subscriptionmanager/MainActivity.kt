fc4064bb7a50da1b694cb0744d4e0dba79c0271b
Dimitri Berardi <Dimitri.Berardi@gmail.com>
2020-10-07 19:15:40 -0400

    Added base functionality and layouts

--- app/src/main/java/com/example/subscriptionmanager/MainActivity.kt
+++ app/src/main/java/com/example/subscriptionmanager/MainActivity.kt
@@ -1,2 +1,34 @@
 package com.example.subscriptionmanager
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    true
+                }
+                R.id.menu_calender -> {
+                    true
+                }
+                R.id.menu_edit_payments -> {
+                    true
+                }
+                R.id.menu_payments_breakdown -> {
+                    true
+                }
+                R.id.menu_view_payments -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
