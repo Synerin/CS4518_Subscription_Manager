beb414d737682e49d5f21488b32321fb8ba7221a
Synerin <36575784+Synerin@users.noreply.github.com>
2020-10-10 16:22:01 -0400

    change getInstance to newInstance

--- app/src/main/java/com/example/subscriptionmanager/BreakdownFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/BreakdownFragment.kt
@@ -1,10 +1,16 @@
 package com.example.subscriptionmanager
 
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
 
 class BreakdownFragment: Fragment() {
+    private lateinit var database: DatabaseReference
+    private lateinit var monthlySpending: TextView
+    private lateinit var expensesList: ListView
 
     companion object {
-        fun getInstance(): BreakdownFragment = BreakdownFragment()
+        fun newInstance(): BreakdownFragment = BreakdownFragment()
     }
 }
