cbfb3a02e42437025da8372dc3276633f9508c27
Synerin <36575784+Synerin@users.noreply.github.com>
2020-10-10 16:16:30 -0400

    add companion object for newInstance

--- app/src/main/java/com/example/subscriptionmanager/BreakdownFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/BreakdownFragment.kt
@@ -1,2 +1,10 @@
 package com.example.subscriptionmanager
 
+import androidx.fragment.app.Fragment
+
+class BreakdownFragment: Fragment() {
+
+    companion object {
+        fun getInstance(): BreakdownFragment = BreakdownFragment()
+    }
+}
