121ef0dd1bd9bcff0611bea2a85f7e288e58b4a6
Synerin <36575784+Synerin@users.noreply.github.com>
2020-10-14 21:11:07 -0400

    lots of date and money formatting

--- app/src/main/java/com/example/subscriptionmanager/HomeFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/HomeFragment.kt
@@ -10,9 +10,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import java.time.temporal.ChronoUnit
 import java.util.*
 
+private const val TAG = "HomeFragment"
 class HomeFragment: Fragment() {
     private lateinit var firedatabase: FirebaseDatabase
     private lateinit var budgetTextView: TextView
@@ -54,6 +54,7 @@ class HomeFragment: Fragment() {
                             subList.add(sub)
                         }
                     }
+                    updateSoonest()
                     updateUpcoming()
                 }
             }
@@ -68,45 +69,116 @@ class HomeFragment: Fragment() {
     }
 
     private fun updateUpcoming() {
-        subList.sortBy { timeFromNow(it.subDueDate) }
+        var expenses: Double = 0.0
+        val calendar: Calendar = Calendar.getInstance()
+        val month: Int = calendar.get(Calendar.MONTH) + 1
+        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+        for(sub in subList) {
+            val dueDate = getNextDue(sub.subDueDate, sub.subFrequency).split("/")
+            val m: Int = dueDate[0].toInt()
+            val d: Int = dueDate[1].toInt()
+            if(m == month && day < d) expenses += sub.subCost.toDouble()
+        }
+
+        upcomingExpenses.text = "$$expenses" // TODO: Format string so cents are accurately represented
+    }
+
+    private fun updateSoonest() {
+        subList.sortBy { timeFromNow(getNextDue(it.subDueDate, it.subFrequency)) }
         val size: Int = subList.size
+        var name: String
+        var due: String
 
         // TODO: Fix
         if(size > 0) {
-            soonestExpenseOne.text = "${subList[0].subName} due on ${subList[0].subDueDate}"
+            name = subList[0].subName
+            due = getNextDue(subList[0].subDueDate, subList[0].subFrequency)
+            soonestExpenseOne.text = "$name due on $due"
         }
         if(size > 1) {
-            soonestExpenseTwo.text = "${subList[1].subName} due on ${subList[1].subDueDate}"
+            name = subList[1].subName
+            due = getNextDue(subList[1].subDueDate, subList[1].subFrequency)
+            soonestExpenseTwo.text ="$name due on $due"
         }
         if(size > 2) {
-            soonestExpenseThree.text = "${subList[2].subName} due on ${subList[2].subDueDate}"
+            name = subList[2].subName
+            due = getNextDue(subList[2].subDueDate, subList[2].subFrequency)
+            soonestExpenseThree.text = "$name due on $due"
         }
         // TODO: Calculate next possible due date for each subscription
     }
 
     private fun timeFromNow(date: String): Double {
-        val month: Int = Calendar.MONTH
-        val day: Int = Calendar.DAY_OF_MONTH
+        val calendar: Calendar = Calendar.getInstance()
+        val month: Int = calendar.get(Calendar.MONTH) + 1
+        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
         val dateValues: List<String> = date.split("/")
-        val givenMonth: Int = dateValues[0].toInt()
-        val givenDay: Int = dateValues[1].toInt()
+        val givenMonth: Double = dateValues[0].toDouble()
+        val givenDay: Double = dateValues[1].toDouble()
 
         var result: Double = 0.0
 
         // I can almost feel that this will cause edge case issues
-        if(month < givenMonth) {
+        if(month <= givenMonth) {
             result += givenMonth - month
         } else {
-            result += (12 - givenMonth) + month
+            result += (12.0 - givenMonth) + month
         }
 
-        if(day < givenDay) {
+        if(day <= givenDay) {
             result += (givenDay - day) / 100
         } else {
-            result += (31 - givenDay) + day // Generalization, may need to change later
+            result += ((31.0 - givenDay) + day) / 100 // Generalization, may need to change later
         }
 
         return result
+    }
+
+    private fun getNextDue(dueDate: String, frequency: String): String {
+        val dateValues: List<String> = dueDate.split("/")
+        val givenMonth: Int = dateValues[0].toInt()
+        val givenDay: Int = dateValues[1].toInt()
+
+        var calendar: Calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        var resultCal: Calendar = Calendar.getInstance()
+
+        when (frequency) {
+            "Yearly" -> {
+                if(month < givenMonth) {
+                    resultCal.set(Calendar.YEAR, year)
+                } else {
+                    resultCal.set(Calendar.YEAR, year + 1)
+                }
+
+                resultCal.set(Calendar.MONTH, givenMonth)
+                resultCal.set(Calendar.DAY_OF_MONTH, givenDay)
+            }
+            "Monthly" -> {
+                if(day < givenDay) {
+                    resultCal.set(Calendar.MONTH, month)
+                } else {
+                    resultCal.set(Calendar.MONTH, month + 1)
+                }
+
+                resultCal.set(Calendar.DAY_OF_MONTH, givenDay)
+            }
+            "Weekly" -> {
+                // TODO: Update values accurately
+                resultCal.set(Calendar.MONTH, givenMonth)
+                resultCal.set(Calendar.DAY_OF_MONTH, givenDay)
+            }
+        }
+
+        val resultDate = "${resultCal.get(Calendar.MONTH)}/" +
+                "${resultCal.get(Calendar.DAY_OF_MONTH)}/" +
+                "${resultCal.get(Calendar.YEAR)}"
+
+        return resultDate
     }
 
     companion object {
